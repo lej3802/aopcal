9e6b9d6b0d08eafd52971cdf37f520a0b54e142e
Eunju Lee <ljhi0222@naver.com>
2018-06-15 09:40:44 +0900

    실행시간 측정하기 3 - 나노시간 측정

--- src/main/java/kr/hs/emirim/ljhi0222/aopcal/main/MainNano.java
+++ src/main/java/kr/hs/emirim/ljhi0222/aopcal/main/MainNano.java
@@ -4,34 +4,29 @@ import kr.hs.emirim.ljhi0222.aopcal.cal.Calculator;
 import kr.hs.emirim.ljhi0222.aopcal.cal.ImpeCalculator;
 import kr.hs.emirim.ljhi0222.aopcal.cal.RecCalculator;
 
-public class MainCall {
+public class MainNano {
 
 	public static void main(String[] args) {
-		int num = 10000;
+		int num = 10;
 		
-		long start1 = System.currentTimeMillis();
+		long start1 = System.nanoTime();
 		
 		Calculator cal1 = new ImpeCalculator();
 		long f1 = cal1.factorial(100);
 		
-		long end1 = System.currentTimeMillis();		
+		long end1 = System.nanoTime();		
 		System.out.printf("IMP factory(%d) : 실행시간 = %d", num, (end1 - start1));
-
-		//System.out.println("IMP 100 : " + f1);
 		
 		
 		System.out.println("\n--------------------------------------------");
 		
 		
-		long start2 = System.currentTimeMillis();
+		long start2 = System.nanoTime();
 		Calculator cal2 = new RecCalculator();
 		long f2 = cal2.factorial(100);
 		
-		long end2 = System.currentTimeMillis();		
-		System.out.printf("IMP factory(%d) : 실행시간 = %d", num, (end2 - start2));
-		
-		//System.out.println("Rec 100 : " + f2);
-
+		long end2 = System.nanoTime();		
+		System.out.printf("REC factory(%d) : 실행시간 = %d", num, (end2 - start2));
 
 	}
 
